3774324fe87b671ba72148ca338db57ec72a14a7
Ashley Watkins <AWatkins31@gmail.com>
2015-08-26 10:43:53 -0700

    Adds full functionality.
    Also removes extra whitespace after line endings.

--- noise.js
+++ noise.js
@@ -1,3 +1,21 @@
 $(document).ready( function() {
-  // your code here
+  function play(letter) {
+    var audio_tag = document.getElementById(letter + 'Audio');
+    audio_tag.currentTime = 0;
+    audio_tag.play();
+  };
+
+  function keyPlay(keycode) {
+    var o = { 99: 'c', 100: 'd', 101: 'e', 102: 'f', 103: 'g', 97: 'a', 98: 'b' };
+    var letter = o[keycode];
+    play(letter);
+  }
+
+  $('.note').click( function() {
+    play( $(this).html() );
+  });
+
+  $(document).keypress( function(k) {
+    keyPlay(k.keyCode);
+  });
 });
